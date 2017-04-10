fc4bb4fed138551bb2a7c52bad686678be828693
Homo Efficio <homo.efficio@gmail.com>
2017-04-10 23:02:38 +0900

    gRPC 서버 서비스 생성
    - 자동 생성된 HelloSpringCampGrpc.HelloSpringCampImplBase 를 상속
    - .proto 파일의 service 내에 rpc로 지정했던 unaryHello()를 Override

---
+++ src/main/java/homo/efficio/springcamp2017/grpc/hello/HelloGrpcServerService.java
@@ -0,0 +1,25 @@
+package homo.efficio.springcamp2017.grpc.hello;
+
+import io.grpc.stub.StreamObserver;
+
+import java.util.logging.Logger;
+
+/**
+ * @author homo.efficio@gmail.com
+ *         created on 2017. 4. 10.
+ */
+public class HelloGrpcServerService extends HelloSpringCampGrpc.HelloSpringCampImplBase {
+
+    private final Logger logger = Logger.getLogger(HelloGrpcServerService.class.getName());
+
+    @Override
+    public void unaryHello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+        logger.info("Unary 메시지 왔다: " + request.getClientName());
+        HelloResponse helloResponse =
+                HelloResponse.newBuilder()
+                             .setWelcomeMessage("Unary Hello " + request.getClientName())
+                             .build();
+        responseObserver.onNext(helloResponse);
+        responseObserver.onCompleted();
+    }
+}
