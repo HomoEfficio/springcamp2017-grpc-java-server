5de2d3a7920d016ffd5d6ee4d8b6302308c8f521
Homo Efficio <homo.efficio@gmail.com>
2017-04-20 18:53:03 +0900

    gRPC 서버 서비스에 clientStreamingHello() 추가

--- src/main/java/homo/efficio/springcamp2017/grpc/hello/HelloGrpcServerService.java
+++ src/main/java/homo/efficio/springcamp2017/grpc/hello/HelloGrpcServerService.java
@@ -2,6 +2,7 @@ package homo.efficio.springcamp2017.grpc.hello;
 
 import io.grpc.stub.StreamObserver;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -82,4 +83,39 @@ public class HelloGrpcServerService extends HelloSpringCampGrpc.HelloSpringCampI
         // 응답 완료
         responseObserver.onCompleted();
     }
+
+    @Override
+    public StreamObserver<HelloRequest> clientStreamingHello(StreamObserver<HelloResponse> responseObserver) {
+        return new StreamObserver<HelloRequest>() {
+            StringBuilder sb = new StringBuilder();
+            @Override
+            public void onNext(HelloRequest request) {
+                logger.info("Client Streaming 메시지 왔다: " + request.getClientName());
+
+                sb.append("Client Streaming Hello " + request.getClientName())
+                  .append("\n============================\n");
+
+                // 1초 동안 비즈니스 로직 처리 후에 응답한다고 가정
+                try { Thread.sleep(1000); } catch (InterruptedException e) { e.printStackTrace(); }
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                logger.log(Level.SEVERE, "Client Streaming requestObserver.onError() 호출");
+            }
+
+            @Override
+            public void onCompleted() {
+                responseObserver.onNext(HelloResponse.newBuilder().setWelcomeMessage(sb.toString()).build());
+                // server는 Streaming이 아니므로 responseObserver.onNext()를 2회 이상 호출할 수 없음.
+                // 아래와 같이 2회 이상 호출하면 responseObserver.onError() 호출됨
+//                responseObserver.onNext(HelloResponse.newBuilder().setWelcomeMessage(sb.toString()).build());
+
+                // 응답 시작 후 1초 후에 응답 완료된다고 가정
+                try { Thread.sleep(1000); } catch (InterruptedException e) { e.printStackTrace(); }
+                responseObserver.onCompleted();
+            }
+        };
+    }
+
 }
